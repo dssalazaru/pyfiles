187c06298df44918798e67c7e897c20641c27c19
dssalazaru <dssalazaru@gmail.com>
2022-02-21 21:33:10 -0500

    Log update and gitignore

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-# ---------------------------------------------------------
-#   Banner and program info - Python Script by dssudev.co
-# ---------------------------------------------------------
+# -------------------------------------------------------- #
+# |   Python File Manager  |  Python Script by dssu.me   | #
+# -------------------------------------------------------- #
 
 # imports 
-import os
+import os, datetime
+
+# ------------------------------------------------------------- #
+date = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")    #
+name, by, ver = "Python File Manager", "@dssalazaru", "v3.0"    #
+br = f"\n{'-'*(len(name) + len(by) + len(ver) + 17)}\n"         #
+# ------------------------------------------------------------- #
 
 # Main function
 def main():
+    # Stating date and log file
+    log(f'[Running {ver}]', ('--> '*15) + '\n')
     # Start program loop
     menu()
     # End of the program
     input("\n # Done, pulse cualquier tecla para salir...")
 
 def banner():
-    global br
-    name, by, ver = "Python File Manager", "@dssalazaru", "v3.0"
-    br = f"\n{'-'*(len(name) + len(by) + len(ver) + 17)}\n"
-    title = f"{br}\t{name} {by} {ver}{br}"
-    print(title)
+    return f"{br}\t{name} {by} {ver}{br}"
     
 def menu():
     opt = 0
-    opcions = """     1) Examinar rutas mayores a 240 caracteres.
+    options = """     1) Examinar rutas mayores a 240 caracteres.
      2) Encontrar Carpetas diferentes entre 2 rutas.
      3) Encontrar TODAS las diferentes entre 2 rutas.
      4) Evaluar tamaño & conteo de Archivos y Carpetas. 
     """
     while(True):
-        banner()
-        print(opcions)
+        print(banner() + options)
         try:
             opt = int(input(" * Proceso a ejecutar > ").strip())
         except Exception: pass
@@ -43,7 +46,7 @@ def menu():
 def opcion1():
     p = Path()
     lf = longFiles(p.data['f'])
-    saveFile("long", lf) # Check duplicates and Save final Log
+    saveFile("long", lf, p.path)
 
 def opcion2():
     print(" Ingrese la Ruta #1")
@@ -53,8 +56,8 @@ def opcion2():
     dir1 = p1.sortData(p1.data['d'])
     dir2 = p2.sortData(p2.data['d'])
     diff = compare(dir1, dir2)
-    saveFile("dir1", diff[0])
-    saveFile("dir2", diff[1])
+    saveFile("dir1", diff[0], p1.path)
+    saveFile("dir2", diff[1], p2.path)
     
 def opcion3():
     print(" Ingrese la Ruta #1")
@@ -64,8 +67,8 @@ def opcion3():
     all1 = p1.sortData(p1.data['all'])
     all2 = p2.sortData(p2.data['all'])
     diff = compare(all1, all2)
-    saveFile("files1", diff[0])
-    saveFile("files2", diff[1])
+    saveFile("files1", diff[0], p1.path)
+    saveFile("files2", diff[1], p2.path)
     
 def opcion4():
     p = Path()
@@ -88,22 +91,24 @@ def compare(l1, l2):
     [lst2.append("Not in [Ruta#2]\t" + i1) for i1 in l1 if i1 not in l2]
     return [lst1, lst2]
 
-def error(func, e):
-    if os.path.isfile('error.log'): mode = 'a'
+def log(func, e):
+    file = 'pyfiles_events.log'
+    if os.path.isfile(file): mode = 'a'
     else: mode = 'w'
-    with open(f"pyfiles_error.log", mode, encoding="utf-8") as f:
-        f.write(f' {func} --> {e}\n')
+    with open(file, mode, encoding="utf-8") as f:
+        f.write(f'[{date}]{func} --> {e}\n')
         f.close
-    input("")
+    print(' ! Error in {}, more info in [{}]'.format(func,file))    
         
-def saveFile(process, lst):
+def saveFile(process, lst, path):
     try:
-        with open(f"pyfiles_{process}.txt", "w", encoding="utf-8") as f: # Create the final file
-            [f.writelines(f"{line}\n") for line in lst] # Save all lines
-    except Exception as e: error("[saveFile]",e)
+        with open(f"pyfiles_{process}.txt", "w", encoding="utf-8") as f:
+            f.write(f'[{date}] {path}\n')
+            [f.writelines(f"{line}\n") for line in lst]
+    except Exception as e: log("[saveFile]",e)
 
 def compile(code):
-    if (code == "DSCode"): os.system('pyinstaller --onefile main.py --name program && pause && exit') # Compile
+    if (code == "DSCode"): os.system(f'pyinstaller --onefile {os.path.realpath(__file__)} --name pyfiles && pause && exit')
 
 class Path():
 
@@ -113,34 +118,36 @@ class Path():
         try:
             tmp = list(map(self.checkDup, [lst for lst in list(self.data.values())]))
             self.data = {'all': tmp[0], 'f': tmp[1], 'd': tmp[2], 's': tmp[3]}
-        except Exception as e: error('[Class: Path] data not fetch', e)
+        except Exception as e: log('[Class: Path] data not fetch', e)
             
     def getPath(self):
         try:
-            while(True): # While the path is blank repeat
+            while(True):
                 path = input(" * Ruta: ").strip()
                 if(path == "."): self.path = os.path.dirname(os.path.realpath(__file__)); break
                 elif(path != ""): self.path = path; break
             print(br)
-        except Exception as e: error("[getPath]",e)
+        except Exception as e: log("[getPath]",e)
         
     def readPath(self):
         al = []; fl = []; dl = []; sz = 0
-        try:
-            for root, directories, files in os.walk(self.path, topdown=False): # Walk for all files 
-                for name in files:
+        for root, directories, files in os.walk(self.path, topdown=False):
+            for name in files:
+                try:
                     f = os.path.join(root, name)
                     fsz = os.path.getsize(f)
                     fl.append(f) 
-                    al.append(str(round((fsz/(1024**2)),2))+"MB\t"+f) # Append all files in general lst
+                    al.append(str(round((fsz/(1024**2)),2))+"MB\t"+f)
                     sz += fsz
-                for name in directories:
+                except Exception as e: log("[readPath : files]",e)
+            for name in directories:
+                try:
                     d = os.path.join(root, name)
                     al.append(d)
                     dl.append(d)
-            sz = [(round(sz/(1024**2),2)), (round(sz/(1024**3),2))]
-            self.data = {'all': al, 'f': fl, 'd': dl, 's': sz}
-        except Exception as e: error("[readPath]",e)
+                except Exception as e: log("[readPath : dirs]",e)
+        sz = [(round(sz/(1024**2),2)), (round(sz/(1024**3),2))]
+        self.data = {'all': al, 'f': fl, 'd': dl, 's': sz}
 
     def sortData(self, lst):
         lst.sort()
@@ -149,12 +156,12 @@ class Path():
     def checkDup(self, lst):
         try:
             if (type(lst) != 'list'): return lst
-            for line in lst: # Read al lines in lst for chech
-                if lst.count(line) > 1: # if are more that one time
-                    print(" * Duplicado: ",line) # Print in console
-                    lst.remove(line) # Remove duplicate entry
-            return lst # Return the new lst
-        except Exception as e: error("[checkDup]",e)
+            for line in lst: 
+                if lst.count(line) > 1:
+                    print(" * Duplicado: ",line)
+                    lst.remove(line)
+            return lst 
+        except Exception as e: log("[checkDup]",e)
     
 # Run program
 if __name__ == "__main__":
